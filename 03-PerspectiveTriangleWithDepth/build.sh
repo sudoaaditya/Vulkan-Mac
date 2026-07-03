6258f6750d4f1509e5c5c473e62d0143589968d7
sudoaaditya <kashidadi@gmail.com>
2026-07-03 17:48:39 +0530

    Folders Updated

--- 03-PerspectiveTriangleWithDepth/build.sh
+++
@@ -1,7 +0,0 @@
-
-rm -rf vk.app
-rm -rf _VulkanWindowLog.txt
-
-mkdir -p vk.app/Contents/MacOS
-
-clang++ -Wno-deprecated-declarations -arch arm64 -o vk.app/Contents/MacOS/vk vk.mm -framework Foundation -framework Cocoa -framework QuartzCore 
