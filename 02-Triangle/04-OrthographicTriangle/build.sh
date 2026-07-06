2f08698b75e2b4caa0d5b99edc034fb92e838a23
sudoaaditya <kashidadi@gmail.com>
2026-07-06 22:11:03 +0530

    Orthographic Triangle Added!

---
+++ 02-Triangle/04-OrthographicTriangle/build.sh
@@ -0,0 +1,7 @@
+
+rm -rf vk.app
+rm -rf _VulkanWindowLog.txt
+
+mkdir -p vk.app/Contents/MacOS
+
+clang++ -Wno-deprecated-declarations -arch arm64 -o vk.app/Contents/MacOS/vk -I $HOME/VulkanSDK/Vulkan/macOS/include -L $HOME/VulkanSDK/Vulkan/macOS/lib -F $HOME/VulkanSDK/Vulkan/macOS/Frameworks -rpath $HOME/VulkanSDK/Vulkan/macOS/Frameworks vk.mm -framework Foundation -framework Cocoa -framework QuartzCore -framework vulkan
